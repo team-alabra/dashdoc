e8f76b989ab11f74bead45220bf953bdc2141979
Alan Alban <alan.alban10@gmail.com>
2024-06-25 22:26:37 -0400

    Added all needed projects and tests

---
+++ Dashdoc.API/Dashdoc.API.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Dashdoc.API.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Dashdoc.API/Dashdoc.API.Insfrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Dashdoc.API.Insfrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
