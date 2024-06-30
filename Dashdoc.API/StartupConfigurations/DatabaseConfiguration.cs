acd0f1a599817f9fba365c0098d34fa471094030
Alan Alban <alan.alban10@gmail.com>
2024-06-30 10:55:40 -0400

    Added db config and RDS settings

--- Dashdoc.API/StartupConfigurations/DatabaseConfiguration.cs
+++ Dashdoc.API/StartupConfigurations/DatabaseConfiguration.cs
@@ -1,7 +1,8 @@
-using Amazon.RDS.Util;
 using Amazon.SecretsManager.Extensions.Caching;
+using Dashdoc.API.Domain.Settings;
 using Dashdoc.API.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Dashdoc.API.Server.StartupConfigurations;
 
@@ -9,14 +10,20 @@ public static class DatabaseConfiguration
 {
     public static IServiceCollection RegisterDashdocDatabase(this IServiceCollection services)
     {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var dbUrl = Environment.GetEnvironmentVariable("DB_URI");
+        var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+
+        Console.WriteLine(env);
+        Console.WriteLine(dbUrl);
+        Console.WriteLine(dbName);
+        
+        string connectionString;
+        
         services.AddDbContext<DashdocDbContext>(options =>
         {
-            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var dbUrl = Environment.GetEnvironmentVariable("DB_URI");
-            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-            var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
-            string connectionString;
-            
+            // Local connection only requires the localhost address and the dBName
+            // remote connection uses AWS Secrets Manager to fetch credentials 
             if (env is null or "Local")
             {
                 connectionString = $"Server={dbUrl}; Database={dbName};";
@@ -25,9 +32,12 @@ public static class DatabaseConfiguration
             {
                 var cache = new SecretsManagerCache();
                 var dbSecretName = Environment.GetEnvironmentVariable("DB_SECRET_NAME");
-                var mySecret = cache.GetSecretString(dbSecretName);
-                connectionString = $"Server={dbUrl};User Id={dbUser};Password={pwd};Database={dbName};";
+                var mySecret = cache.GetSecretString(dbSecretName).GetAwaiter().GetResult();
+                var rds = JsonSerializer.Deserialize<RdsAppSettings>(mySecret);
+                
+                connectionString = $"Server={rds?.Host};User Id={rds?.Username};Password={rds?.Password};Database={rds?.DbName};";
             }
+            
             options.UseNpgsql(connectionString);
         });
         
