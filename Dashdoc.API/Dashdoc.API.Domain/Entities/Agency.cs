c35714caba45ab9311cea0b5b578bb641cb22078
Alan Alban <alan.alban10@gmail.com>
2024-08-28 13:47:01 -0400

    Inherited class

--- Dashdoc.API/Dashdoc.API.Domain/Entities/Agency.cs
+++ Dashdoc.API/Dashdoc.API.Domain/Entities/Agency.cs
@@ -7,14 +7,24 @@ public class Agency
 {
     [Column("id")]
     public long Id { get; set; }
+    [Column("email")]
     public string? Email { get; set; }
+    [Column("name")]
     public string? Name { get; set; }
+    [Column("phone_number")]
     public string? PhoneNumber { get; set; }
+    [Column("secondary_number")]
     public string? SecondaryNumber { get; set; }
+    [Column("street_address")]
     public string? StreetAddress { get; set; }
+    [Column("city")]
     public string? City { get; set; }
+    [Column("state")]
     public string? State { get; set; }
+    [Column("zip_code")]
     public string? ZipCode { get; set; }
+    [Column("created_on")]
     public DateTime CreatedOn { get; set; }
+    [Column("last_updated")]
     public DateTime LastUpdated { get; set; }
 }
--- Dashdoc.API/Dashdoc.API.Domain/Models/Agency.cs
+++ Dashdoc.API/Dashdoc.API.Domain/Models/Agency.cs
@@ -1,15 +1,10 @@
+using Dashdoc.API.Domain.Abstract;
+
 namespace Dashdoc.API.Domain.Models;
 
-public class Agency
+public class Agency: Party
 {
-    public long Id { get; set; }
-    public string? Email { get; set; }
     public string? Name { get; set; }
     public string? PhoneNumber { get; set; }
     public string? SecondaryNumber { get; set; }
-    public string? StreetAddress { get; set; }
-    public string? City { get; set; }
-    public string? State { get; set; }
-    public string? ZipCode { get; set; }
-    public DateTime CreatedOn { get; set; } // Maybe can be used to show how long the agency has been with us
 }
