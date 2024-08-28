eb588a214c8eac48a83102a886b34f84d1aa82fb
Alan Alban <alan.alban10@gmail.com>
2024-08-28 15:25:52 -0400

    Added logo URL property

--- Dashdoc.API/Dashdoc.API.Domain/Entities/Agency.cs
+++ Dashdoc.API/Dashdoc.API.Domain/Entities/Agency.cs
@@ -1,9 +1,10 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Dashdoc.API.Domain.Abstract;
 
 namespace Dashdoc.API.Domain.Entities;
 
 [Table("agency")]
-public class Agency
+public class Agency: BaseEntity
 {
     [Column("id")]
     public long Id { get; set; }
@@ -11,6 +12,8 @@ public class Agency
     public string? Email { get; set; }
     [Column("name")]
     public string? Name { get; set; }
+    [Column("logo_url")]
+    public string? LogoUrl { get; set; }
     [Column("phone_number")]
     public string? PhoneNumber { get; set; }
     [Column("secondary_number")]
@@ -23,8 +26,4 @@ public class Agency
     public string? State { get; set; }
     [Column("zip_code")]
     public string? ZipCode { get; set; }
-    [Column("created_on")]
-    public DateTime CreatedOn { get; set; }
-    [Column("last_updated")]
-    public DateTime LastUpdated { get; set; }
 }
--- Dashdoc.API/Dashdoc.API.Domain/Models/Agency.cs
+++ Dashdoc.API/Dashdoc.API.Domain/Models/Agency.cs
@@ -5,6 +5,7 @@ namespace Dashdoc.API.Domain.Models;
 public class Agency: Party
 {
     public string? Name { get; set; }
+    public string? LogoUrl { get; set; }
     public string? PhoneNumber { get; set; }
     public string? SecondaryNumber { get; set; }
 }
