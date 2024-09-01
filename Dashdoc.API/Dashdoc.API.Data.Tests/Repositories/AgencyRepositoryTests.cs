1c96489299c3b3eeee14b2045a804adc457aedee
Alan Alban <alan.alban10@gmail.com>
2024-09-01 15:53:03 -0400

    Added mor eunit tests

--- Dashdoc.API/Dashdoc.API.Data.Tests/Repositories/AgencyRepositoryTests.cs
+++ Dashdoc.API/Dashdoc.API.Data.Tests/Repositories/AgencyRepositoryTests.cs
@@ -7,9 +7,10 @@ namespace Dashdoc.API.Data.Tests.Repositories;
 public class AgencyRepositoryTests
 {
     public AgencyRepositoryTests() { }
-
+    
+    // POST and GET
     [Fact]
-    public async Task GetById_ReturnsEntity()
+    public async Task GetByIdAsync_Creates_And_ReturnsEntity()
     {
         // Arrange
         var factory = new TestDBContextFactory();
@@ -28,5 +29,54 @@ public class AgencyRepositoryTests
         // Assert
         Assert.NotNull(result);
         Assert.Equal(agencyId, result?.Id);
+    }
+    
+    // PUT
+    [Fact]
+    public async Task UpdateAsync_ReturnsEntity()
+    {
+        // Arrange
+        var factory = new TestDBContextFactory();
+        var context = factory.CreateContext();
+        
+        var agencyRepository = new AgencyRepository(context); 
+        
+        // Act
+        var fakeAgency = AgencyFactory.GetFaker().Generate(1).First(); // Generate returns an array even if you only generate one item, this is why I use first after to get the first element of the array.
+        var agencyId = fakeAgency.Id;
+        context.Agency.Add(fakeAgency); // This line saves our entity to the in-memory database
+        context.SaveChanges();
+
+        fakeAgency.Name = "Test Update 1";
+        
+        var result = await agencyRepository.UpdateAsync(fakeAgency);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Test Update 1", result?.Name);
+    }
+    
+    // PUT
+    [Fact]
+    public async Task DeleteAsync_EntityCannotBeFound()
+    {
+        // Arrange
+        var factory = new TestDBContextFactory();
+        var context = factory.CreateContext();
+        
+        var agencyRepository = new AgencyRepository(context); 
+        
+        // Act
+        var fakeAgency = AgencyFactory.GetFaker().Generate(1).First(); // Generate returns an array even if you only generate one item, this is why I use first after to get the first element of the array.
+        var agencyId = fakeAgency.Id;
+        context.Agency.Add(fakeAgency); // This line saves our entity to the in-memory database
+        context.SaveChanges();
+        
+        var result = await agencyRepository.DeleteAsync(agencyId);
+        var entity = await agencyRepository.GetByIdAsync(agencyId);
+        
+        // Assert
+        Assert.True(result);
+        Assert.Null(entity);
     }
 }
