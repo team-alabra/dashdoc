0629a59e659e5d480fcda695ddfdd5f4f071e135
Alan Alban <alan.alban10@gmail.com>
2024-09-01 15:55:31 -0400

    Changed provider name in db context

--- Dashdoc.API/Dashdoc.API.Data/DashdocDbContext.cs
+++ Dashdoc.API/Dashdoc.API.Data/DashdocDbContext.cs
@@ -8,6 +8,6 @@ public class DashdocDbContext: DbContext
     public DashdocDbContext() { }
     public DashdocDbContext(DbContextOptions<DashdocDbContext> options): base(options) { }
     
-    public virtual DbSet<Provider> TestEntity { get; init; }
+    public virtual DbSet<Provider> Provider { get; init; }
     public virtual DbSet<Agency> Agency { get; init; }
 }
