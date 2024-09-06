91c31945cf7b952004ec67b7838f2d08a9b714b0
Alan Alban <alan.alban10@gmail.com>
2024-09-06 12:39:48 -0400

    Added authorize attributes for testing

--- Dashdoc.API/Dashdoc.API.Server/Controllers/TestController.cs
+++ Dashdoc.API/Dashdoc.API.Server/Controllers/TestController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Dashdoc.API.Domain.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,18 +7,33 @@ namespace Dashdoc.API.Server.Controllers;
 
 [ApiController]
 [Route("/api/test")]
+[Authorize]
 public class TestController: ControllerBase
 {
     public TestController() { }
 
     // TODO - replace with real Provider routes
     [HttpGet]
-    [Authorize(Roles = UserRoles.AgencyAdministrator)]
-    public ActionResult Get()
+    public ActionResult GetV1()
     {
         try
         {
             return Ok("Success");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return Problem("Problem retrieving Data");
+        }
+    }
+    
+    [HttpGet("admin")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public ActionResult GetV2([FromHeader(Name = "x-user-id")] long userId)
+    {
+        try
+        {
+            return Ok($"UserId is {userId}");
         }
         catch (Exception ex)
         {
